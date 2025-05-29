071bf65510c6ef468713c3287faa9bc62f307a59
Stéphane Ravet <ravet.st@gmail.com>
2025-05-29 09:58:22 +0200

    Mise à jour du fichier __manifest__.py pour utiliser des guillemets doubles et changement de la licence en MIT.

--- sale_order_import_batch/__manifest__.py
+++ sale_order_import_batch/__manifest__.py
@@ -1,17 +1,17 @@
 {
-    'name': 'Sale Order Import Batch',
-    'version': '1.0.0',
-    'summary': 'Batch import of orders',
-    'category': 'Tools',
-    'author': 'Stéphane Ravet',
-    'website': 'https://www.linkedin.com/in/stéphane-ravet/',
-    'depends': ['base', 'sale'],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/ir.config_parameter.csv',
+    "name": "Sale Order Import Batch",
+    "version": "1.0.0",
+    "summary": "Batch import of orders",
+    "category": "Tools",
+    "author": "Stéphane Ravet",
+    "website": "https://www.linkedin.com/in/stéphane-ravet/",
+    "depends": ["base", "sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/ir.config_parameter.csv",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-    'license': 'LGPL-3',
-} 
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "MIT",
+}
